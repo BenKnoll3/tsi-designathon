128aea829d67b71cbd28e05119535fb957f26c49
Ben Knoll <bknoll@ewu.edu>
2024-11-24 22:34:55 -0800

    Finished Product
    * Use vuetify v-app-bar for a place holder nav bar
    * The directory itself is built using a v-data-table wrapped in a v-card
    * Hacked up some data cleaning for the names, pictures, dates hired, and phone numbers

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,77 @@
+<style></style>
 <template>
-  Hello, world!
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>Trans-System</v-app-bar-title>
+  </v-app-bar>
+  <v-card title="Employee Directory" class="w-75 mt-10 mx-auto">
+    <template v-slot:text>
+      <v-text-field
+        v-model="search"
+        label="Search by name"
+        prepend-inner-icon="mdi-magnify"
+        variant="outlined"
+        hide-details
+        single-line
+      ></v-text-field>
+    </template>
+
+    <v-data-table :headers="headers" :items="payload" :search="search">
+      <template v-slot:item.thumbnailURI="{ value }">
+        <v-avatar size="large">
+          <v-img v-bind:src="value"> </v-img>
+        </v-avatar>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script setup lang="ts">
-const payload = usePayload()
+const payload = usePayload();
+const search = ref("");
+
+const date = useDate();
+
+const headers = ref([
+  {
+    key: "thumbnailURI",
+    value: (item) =>
+      `${
+        item.thumbnailURI
+          ? item.thumbnailURI
+          : "https://www.svgrepo.com/show/335455/profile-default.svg"
+      }`,
+  },
+  {
+    title: "Name",
+    key: "fullName",
+    value: (item) =>
+      `${
+        item.firstName.charAt(0).toUpperCase() +
+        item.firstName.substring(1, item.firstName.length).toLowerCase()
+      } ${item.lastName.toUpperCase()}`,
+  },
+  {
+    title: "Department",
+    key: "department",
+    value: (item) => `${item.department ? item.department : "-"}`,
+  },
+  { title: "Position", key: "jobTitle" },
+
+  {
+    title: "Phone Number",
+    key: "phone",
+    value: (item) =>
+      `${item.phone.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3")}`,
+  },
+  { title: "Email Address", key: "email", value: "email" },
+  {
+    title: "Date Hired",
+    key: "dateHired",
+    value: (item) => `${date.format(item.dateHired, "fullDate")}`,
+  },
+]);
 </script>
